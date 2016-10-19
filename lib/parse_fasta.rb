ece7b737bae75a4ff491da19ed933f536de459ed
Ryan Moore <moorer@udel.edu>
2016-10-19 00:10:59 -0400

    Clean house

--- lib/parse_fasta.rb
+++ lib/parse_fasta.rb
@@ -1,40 +1,5 @@
-# Copyright 2014, 2015 Ryan Moore
-# Contact: moorer@udel.edu
-#
-# This file is part of parse_fasta.
-#
-# parse_fasta is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# parse_fasta is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with parse_fasta.  If not, see <http://www.gnu.org/licenses/>.
-
-require 'parse_fasta/version'
-require 'parse_fasta/fasta_file'
-require 'parse_fasta/fastq_file'
-require 'parse_fasta/seq_file'
-require 'parse_fasta/sequence'
-require 'parse_fasta/quality'
+require "parse_fasta/version"
 
 module ParseFasta
-
-  class Error < StandardError
-  end
-
-  # Error raised when FASTA file is malformed
-  class DataFormatError < IOError
-    def message
-      "Data format error -- check input file"
-    end
-  end
-
-  class SequenceFormatError < Error
-  end
+  # Your code goes here...
 end
