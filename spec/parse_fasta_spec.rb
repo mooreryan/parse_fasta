e666aafb433ac251154b5bd7936cc0a6b6f64b38
Ryan Moore <moorer@udel.edu>
2016-10-19 00:17:31 -0400

    Add license

--- spec/parse_fasta_spec.rb
+++ spec/parse_fasta_spec.rb
@@ -1,11 +1,25 @@
+# Copyright 2014 - 2016 Ryan Moore
+# Contact: moorer@udel.edu
+#
+# This file is part of parse_fasta.
+#
+# parse_fasta is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# parse_fasta is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with parse_fasta.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'spec_helper'
 
 describe ParseFasta do
   it 'has a version number' do
     expect(ParseFasta::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
