305291d563d01daa224a788e6c2dd4929ef96923
Ryan Moore <moorer@udel.edu>
2018-03-17 15:54:26 -0400

    Add remove_gaps

--- lib/parse_fasta/core_ext/string.rb
+++ lib/parse_fasta/core_ext/string.rb
@@ -16,12 +16,31 @@
 # You should have received a copy of the GNU General Public License
 # along with parse_fasta.  If not, see <http://www.gnu.org/licenses/>.
 
-require "parse_fasta/core_ext/string"
-
-require "parse_fasta/version"
-require "parse_fasta/record"
-require "parse_fasta/seq_file"
-require "parse_fasta/error"
-
 module ParseFasta
+  module CoreExt
+    module String
+
+      # Removes all gap chars from the string.
+      #
+      # @example Remove all '-' from string
+      #   # First inclued the methods
+      #   String.include ParseFasta::CoreExt::String
+      #
+      #   "--A-C-t-g".remove_gaps #=> "ACtg"
+      #
+      # @example Change the gap character to 'n'
+      #   # First inclued the methods
+      #   String.include ParseFasta::CoreExt::String
+      #
+      #   "-N-nACTG".remove_gaps "N" #=> "--nACTG"
+      #
+      # @param gap_char [String] the character to treat as a gap
+      #
+      # @return [String] a string with all instances of
+      #   gap_char_removed
+      def remove_gaps gap_char="-"
+        self.tr gap_char, ""
+      end
+    end
+  end
 end
--- spec/parse_fasta/core_ext/string_spec.rb
+++ spec/parse_fasta/core_ext/string_spec.rb
@@ -16,12 +16,35 @@
 # You should have received a copy of the GNU General Public License
 # along with parse_fasta.  If not, see <http://www.gnu.org/licenses/>.
 
-require "parse_fasta/core_ext/string"
-
-require "parse_fasta/version"
-require "parse_fasta/record"
-require "parse_fasta/seq_file"
-require "parse_fasta/error"
+require "spec_helper"
 
 module ParseFasta
+  module CoreExt
+    describe String do
+      Object::String.include ParseFasta::CoreExt::String
+
+      describe "#remove_gaps" do
+        it "removes all gaps from alignment" do
+          aln = "--A----C-tG---"
+          expected = "ACtG"
+
+          expect(aln.remove_gaps).to eq expected
+        end
+
+        it "returns empty string if all chars are gap chars" do
+          aln = "-----"
+          expected = ""
+
+          expect(aln.remove_gaps).to eq expected
+        end
+
+        it "can take a different gap char" do
+          aln = "-N-nACTG"
+          expected = "--nACTG"
+
+          expect(aln.remove_gaps "N").to eq expected
+        end
+      end
+    end
+  end
 end
